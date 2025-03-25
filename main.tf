55c3b90548c966dee2e928e4f89d582e82d60ce3
Wiame Chliah <95120188+Wima78@users.noreply.github.com>
2025-03-25 08:56:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,36 @@
-provider "aws" {
-  region = var.region
-}
+terraform {  
+  required_providers {  
+    aws = {  
+      source  = "hashicorp/aws"  
+      version = "4.15.0"  
+    }  
+  }  
+}  
 
-resource "aws_instance" "citadel" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = aws_key_pair.citadel-key.key_name
-  user_data     = file("/root/terraform-challenges/project-citadel/install-nginx.sh")
-  
-  tags = {
-    Name = "citadel"
-  }
-}
+provider "aws" {  
+  region                        = var.region  
+  skip_credentials_validation   = true  
+  skip_requesting_account_id    = true  
+}  
 
-resource "aws_key_pair" "citadel-key" {
-  key_name   = "citadel-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Assurez-vous d'avoir une clé SSH générée
-}
+endpoints {  
+  ec2                 = "http://aws:4566"  
+  apigateway          = "http://aws:4566"  
+  cloudformation      = "http://aws:4566"  
+  cloudwatch          = "http://aws:4566"  
+  dynamodb           = "http://aws:4566"  
+  es                  = "http://aws:4566"  
+  firehose            = "http://aws:4566"  
+  iam                 = "http://aws:4566"  
+  kinesis             = "http://aws:4566"  
+  lambda              = "http://aws:4566"  
+  route53             = "http://aws:4566"  
+  redshift            = "http://aws:4566"  
+  s3                  = "http://aws:4566"  
+  secretsmanager      = "http://aws:4566"  
+  sns                 = "http://aws:4566"  
+  sqs                 = "http://aws:4566"  
+  ssm                 = "http://aws:4566"  
+  stepfunctions       = "http://aws:4566"  
+  sts                 = "http://aws:4566"  
+}  
